ce5879f64e6ec8d288abfe890ca2389d4ffa8a35
James Ward <jimmywardmichael@gmail.com>
2023-11-29 09:26:32 -0600

    initial coffee house setup

---
+++ src/main/java/com/example/coffeehouseproject/CoffeeHouseProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.coffeehouseproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeHouseProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeHouseProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/coffeehouseproject/CoffeeHouseProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coffeehouseproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeHouseProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
